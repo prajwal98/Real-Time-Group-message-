795218b42127bea62c9c512909c303a79ebdff71
Dev <ursprajwal1@gmail.com>
2021-04-29 11:07:54 +0530

    version 1.0.0 cleanup

--- server.js
+++ server.js
@@ -164,6 +164,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 80 || 443 || 3000;
+const PORT = process.env.PORT || 443 || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
